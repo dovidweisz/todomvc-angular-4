da40bf2fa04fa89c49bbb344fb7c581e6f9bc2b6
dovidweisz <dave@dweisz.com>
2017-06-13 22:47:12 -0400

    fix build after sass-loader upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,36 @@ const baseHref = "";
 const deployUrl = "";
 
 
+const sassLoaderOptions = {
+  "sourceMap": false,
+  "includePaths": [path.join(process.cwd(), "src/scss")]
+};
 
+const postcssLoaderOptions = {
+  plugins: [
+    autoprefixer(),
+    postcssUrl({"url": (URL) => {
+      // Only convert root relative URLs, which CSS-Loader won't process into require().
+      if (!URL.startsWith('/') || URL.startsWith('//')) {
+          return URL;
+      }
+      if (deployUrl.match(/:\/\//)) {
+          // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
+          return `${deployUrl.replace(/\/$/, '')}${URL}`;
+      }
+      else if (baseHref.match(/:\/\//)) {
+          // If baseHref contains a scheme, include it as is.
+          return baseHref.replace(/\/$/, '') +
+              `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+      }
+      else {
+          // Join together base-href, deploy-url and the original URL.
+          // Also dedupe multiple slashes into single ones.
+          return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+      }
+  }})
+  ],
+};
 
 module.exports = {
   "devtool": "source-map",
@@ -84,7 +113,7 @@ module.exports = {
         "loaders": [
           "exports-loader?module.exports.toString()",
           "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader"
+          `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
         ]
       },
       {
@@ -92,11 +121,22 @@ module.exports = {
           path.join(process.cwd(), "src/styles.css")
         ],
         "test": /\.scss$|\.sass$/,
-        "loaders": [
-          "exports-loader?module.exports.toString()",
-          "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader",
-          "sass-loader"
+        "use": [
+          {
+            loader: "exports-loader?module.exports.toString()",
+          },
+          {
+            loader: "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
+          },
+          {
+            loader: `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
+          },
+          {
+            loader: `sass-loader`,
+            options: sassLoaderOptions
+          },
+
+
         ]
       },
       {
@@ -107,7 +147,7 @@ module.exports = {
         "loaders": [
           "exports-loader?module.exports.toString()",
           "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader",
+          `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
           "less-loader"
         ]
       },
@@ -119,7 +159,7 @@ module.exports = {
         "loaders": [
           "exports-loader?module.exports.toString()",
           "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-          "postcss-loader",
+          `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
           "stylus-loader?{\"sourceMap\":false,\"paths\":[]}"
         ]
       },
@@ -131,7 +171,7 @@ module.exports = {
         "loaders": ExtractTextPlugin.extract({
   "use": [
     "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader"
+    `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
   ],
   "fallback": "style-loader",
   "publicPath": ""
@@ -145,8 +185,8 @@ module.exports = {
         "loaders": ExtractTextPlugin.extract({
   "use": [
     "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
-    "sass-loader"
+    `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
+    `sass-loader?${JSON.stringify(sassLoaderOptions)}`,
   ],
   "fallback": "style-loader",
   "publicPath": ""
@@ -160,7 +200,7 @@ module.exports = {
         "loaders": ExtractTextPlugin.extract({
   "use": [
     "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
+    `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
     "less-loader"
   ],
   "fallback": "style-loader",
@@ -175,7 +215,7 @@ module.exports = {
         "loaders": ExtractTextPlugin.extract({
   "use": [
     "css-loader?{\"sourceMap\":false,\"importLoaders\":1}",
-    "postcss-loader",
+    `postcss-loader?${JSON.stringify(postcssLoaderOptions)}`,
     "stylus-loader?{\"sourceMap\":false,\"paths\":[]}"
   ],
   "fallback": "style-loader",
@@ -249,33 +289,6 @@ module.exports = {
     new LoaderOptionsPlugin({
       "sourceMap": false,
       "options": {
-        "postcss": [
-          autoprefixer(),
-          postcssUrl({"url": (URL) => {
-            // Only convert root relative URLs, which CSS-Loader won't process into require().
-            if (!URL.startsWith('/') || URL.startsWith('//')) {
-                return URL;
-            }
-            if (deployUrl.match(/:\/\//)) {
-                // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
-                return `${deployUrl.replace(/\/$/, '')}${URL}`;
-            }
-            else if (baseHref.match(/:\/\//)) {
-                // If baseHref contains a scheme, include it as is.
-                return baseHref.replace(/\/$/, '') +
-                    `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-            }
-            else {
-                // Join together base-href, deploy-url and the original URL.
-                // Also dedupe multiple slashes into single ones.
-                return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-            }
-        }})
-        ],
-        "sassLoader": {
-          "sourceMap": false,
-          "includePaths": [path.join(process.cwd(), "src/scss")]
-        },
         "lessLoader": {
           "sourceMap": false
         },
